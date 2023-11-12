492f686531f6d1dd099fd9fb82c8367d0a88a0a6
unknown <tirthamunai1234@gmail.com>
2023-11-12 16:49:40 +0530

    Tirtha

--- src/components/SocialLinksSlider.jsx
+++ src/components/SocialLinksSlider.jsx
@@ -1,17 +1,17 @@
-// SocialLinksSlider.js
+
 import React from 'react';
 import instagram from "../images/instagram.png"
 import facebook from "../images/facebook.png"
 import linkedin from "../images/linkedin.png"
 import twitter from "../images/twitter.png"
-import resume from "../images/resume.png"
+import github from "../images/github.png"
 const SocialLinksSlider = () => {
   const socialLinks = [
     { id: 1, url: 'https://www.linkedin.com/in/tirtha-biswas-594581230/',src: linkedin},
     { id: 2, url: 'https://www.instagram.com/_tirtha_18/',src: instagram},
     { id: 3, url: 'https://www.facebook.com/profile.php?id=100079907761236',src: facebook},
     { id: 4, url: 'https://twitter.com/TirthaBiswas18',src: twitter},
-    { id: 5, url: 'https://docs.google.com/document/d/1Q-WIcmT8qptMBJF4nEK-izARISEMLjwY8lL-Nx_gcao/edit',src: resume}
+    { id: 5, url: 'https://github.com/tirtha18',src: github}
   ];
 
   return (
