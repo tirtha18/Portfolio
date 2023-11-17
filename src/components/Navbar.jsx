5d1e142318039fba10499bceec357489bab14e8c
unknown <tirthamunai1234@gmail.com>
2023-11-17 13:26:42 +0530

    Changes for mobver

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,7 +49,7 @@ export default function Navbar() {
         {nav && (
         <ul className="flex flex-col justify-centre items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to bg-gray-600">
                 {links.map(({id , link}) => (<li key={id} className="px-4 cursor-pointer capitalize text-4xl py-6"><Link onClick={()=>setNav(!nav)}to={link} smooth duration={500}>{link}</Link></li>))}
-                <a href="https://docs.google.com/document/d/1Q-WIcmT8qptMBJF4nEK-izARISEMLjwY8lL-Nx_gcao/edit">
+                <a href={My_Resume} target="_blabk">
                     <li className="px-4 cursor-pointer capitalize text-4xl py-6">
                         resume
                     </li>
